bcc5e65bab8e60a3eb659da604cf1002e6ba1e0f
saphalpantha <saphalpantha@gmail.com>
2025-06-17 14:23:02 +0545

    Added DB prototype

---
+++ db/manage.py
@@ -0,0 +1,21 @@
+from typing import List, Dict
+class DBManager:
+    def __init__(self):
+        # connect to db (SQLite for now)
+        return;
+    def insert_commits(self, data:List[Dict]):
+        # bulk insert
+        return
+
+    def insert_contributors(self, data: List[Dict]):
+        # bulk insert
+        return
+
+    def insert_repo_meta(self, data: Dict):
+        # insert/update
+        return
+
+    def insert_commit(self, data:Dict):
+        return
+        # aposd
+
